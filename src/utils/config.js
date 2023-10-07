2353b08328f8950bfb3e11354a48611bbd72f8af
Bhagyashri <bhagyashrideoghare116@gmail.com>
2023-10-07 12:34:13 +0530

    done

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@ export const api_options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'eyJhbGc iOiJIUzI1NiJ9.eyJhdWQiOiI1YzdmOTY1MmRkM2M3MTZjYmJiZDllNjE3ODg5MDliMiIsInN1YiI6IjY0ZjE3Mzk4ZGJiYjQyMDExYjcxMDk4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qymmX3vytfwWvxttuAU30VU99Kgqre6Fnq-1HFr6rR8'
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YzdmOTY1MmRkM2M3MTZjYmJiZDllNjE3ODg5MDliMiIsInN1YiI6IjY0ZjE3Mzk4ZGJiYjQyMDExYjcxMDk4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qymmX3vytfwWvxttuAU30VU99Kgqre6Fnq-1HFr6rR8'
     }
   };
   
